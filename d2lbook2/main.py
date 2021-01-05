1a9ff8e42f54ca9beea195c55cc569fe23a6e4c0
stevenjokes <a867907127@gmail.com>
2021-01-06 02:26:19 +0800

    Run d2lbook2 command

--- d2lbook2/main.py
+++ d2lbook2/main.py
@@ -7,7 +7,7 @@ from d2lbook2.activate import activate
 from d2lbook2.translate import translate
 import logging
 
-logging.basicConfig(format='[d2lbook:%(filename)s:L%(lineno)d] %(levelname)-6s %(message)s')
+logging.basicConfig(format='[d2lbook2:%(filename)s:L%(lineno)d] %(levelname)-6s %(message)s')
 logging.getLogger().setLevel(logging.INFO)
 
 
@@ -17,7 +17,7 @@ def main():
     parser = argparse.ArgumentParser(description='''
 D2L Book: Publish a book based on Jupyter notebooks.
 
-Run d2lbook command -h to get the help message for each command.
+Run d2lbook2 command -h to get the help message for each command.
 ''')
     parser.add_argument('command', nargs=1, choices=commands.keys())
     args = parser.parse_args(sys.argv[1:2])
